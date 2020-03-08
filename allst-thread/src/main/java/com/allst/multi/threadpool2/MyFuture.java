6ce1540a9152dcad09902802cd9abc9dfc2da0e5
returntears <returntears@outlook.com>
2020-03-08 16:37:36 +0800

    Executors获取ExecutorService 异步计算结果Futrue

---
+++ allst-thread/src/main/java/com/allst/multi/threadpool2/MyFuture.java
@@ -0,0 +1,48 @@
+package com.allst.multi.threadpool2;
+
+import java.util.concurrent.*;
+
+/**
+ * 异步计算结果
+ * @author YiYa
+ * @since 2020-03-08 下午 04:29
+ */
+public class MyFuture {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // 创建线程池
+        ExecutorService es = Executors.newCachedThreadPool();
+        // 创建callable类型的任务对象
+        Future<Integer> future = es.submit(new MyCall(1, 7));
+        boolean done = future.isDone();
+        System.out.println("任务是否完成~ " + done);
+        boolean rs = future.isCancelled();
+        System.out.println("任务是否取消~ " + rs);
+        // 一致等待任务的执行， 直到任务完成
+        Integer v = future.get();
+        System.out.println("任务执行结果: " + v);
+        boolean isd = future.isDone();
+        System.out.println("任务是否完成~ " + isd);
+    }
+
+}
+
+class MyCall implements Callable<Integer> {
+
+    private int a;
+    private int b;
+
+    public MyCall(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        String name = Thread.currentThread().getName();
+        System.out.println("计算开始~~~");
+        Thread.sleep(2000);
+        System.out.println("计算完成~~~");
+        return a + b;
+    }
+}
--- allst-thread/src/main/java/com/allst/multi/threadpool2/MyPoolExecutors.java
+++ allst-thread/src/main/java/com/allst/multi/threadpool2/MyPoolExecutors.java
@@ -1,8 +1,7 @@
 package com.allst.multi.threadpool2;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * Executors获取ExecutorService
@@ -13,7 +12,7 @@ import java.util.concurrent.ThreadFactory;
 public class MyPoolExecutors {
 
     public static void main(String[] args) {
-        meth6();
+        meth9();
     }
 
     private static void meth1() {
@@ -57,10 +56,16 @@ public class MyPoolExecutors {
     }
 
     private static void meth5() {
+        // 使用线程工厂获取线程池对象
         ExecutorService es = Executors.newSingleThreadExecutor();
+        // 提交任务
         for (int i = 0; i < 10; i++) {
             es.submit(new MtRunnable(i + 1));
         }
+        // 关闭线程池,仅仅时不能接受新的任务,以前的任务还是会继续执行
+        es.shutdown();
+        // 关闭线程池后，不能再提交新的任务了
+        // es.submit(new MtRunnable(22));
     }
 
     private static void meth6() {
@@ -71,11 +76,51 @@ public class MyPoolExecutors {
                 return new Thread(r, "自定义SingleT线程" + n++);
             }
         });
+        // 提交任务
         for (int i = 0; i < 10; i++) {
             es.submit(new MtRunnable(i + 1));
         }
+        // 关闭线程池， 如果线程池中有缓存的任务没有执行， 则取消执行并返回
+        List<Runnable> list = es.shutdownNow();
+        System.out.println(list);
     }
 
+
+    private static void meth7() {
+        ScheduledExecutorService ses = Executors.newScheduledThreadPool(3);
+        for (int i = 0; i < 10; i++) {
+            ses.schedule(new MtRunnable(i + 1), 2, TimeUnit.SECONDS);
+        }
+        System.out.println("it`s over~~~");
+    }
+
+    private static void meth8() {
+        ScheduledExecutorService ses = Executors.newScheduledThreadPool(3, new ThreadFactory() {
+            int i = 0;
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r, "自定义线程Scheduled" + i++);
+            }
+        });
+
+        ses.scheduleAtFixedRate(new MtRunnable( 1), 1, 2, TimeUnit.SECONDS);
+
+        System.out.println("it`s over~~~");
+    }
+
+    private static void meth9() {
+        ScheduledExecutorService ses = Executors.newScheduledThreadPool(3, new ThreadFactory() {
+            int i = 0;
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r, "自定义线程Scheduled" + i++);
+            }
+        });
+
+        ses.scheduleWithFixedDelay(new MtRunnable( 1), 1, 2, TimeUnit.SECONDS);
+
+        System.out.println("it`s over~~~");
+    }
 }
 
 class MtRunnable implements Runnable {
@@ -84,6 +129,13 @@ class MtRunnable implements Runnable {
 
     public MtRunnable(int id) {
         this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "MtRunnable{" +
+                "id=" + id +
+                '}';
     }
 
     @Override
