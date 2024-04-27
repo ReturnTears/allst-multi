be7a765e2450190761b0a46ef063747f32d5cee0
returntears <returntears@outlook.com>
2024-04-27 21:08:37 +0800

    java StampedLock

---
+++ allst-jmh/src/main/java/com/allst/jmh/tools/StampedLockExample3.java
@@ -0,0 +1,47 @@
+package com.allst.jmh.tools;
+
+import java.util.concurrent.locks.StampedLock;
+
+/**
+ * 乐观读模式
+ *
+ * @author Hutu
+ * @since 2024-04-27 下午 08:56
+ */
+public class StampedLockExample3 {
+    // 共享数据
+    private static int shareData = 0;
+
+    // 定义StampedLock 锁
+    private static final StampedLock lock = new StampedLock();
+
+    private static void inc() {
+        // 调用writeLock方法返回一个数据stamp
+        long stamp = lock.writeLock();
+        try {
+            // 修改共享数据
+            shareData++;
+        } finally {
+            // 释放锁
+            lock.unlockWrite(stamp);
+        }
+    }
+
+    private static int get() {
+        // 获取读锁，并记录数据戳stamp
+        // tryOptimisticRead()该方法为立即返回方法，并不会导致当前线程进入阻塞等待
+        long stamp = lock.tryOptimisticRead();
+        if (!lock.validate(stamp)) {
+            // 获取读锁
+            stamp = lock.readLock();
+            try {
+                // 返回数据
+                return shareData;
+            } finally {
+                // 释放锁
+                lock.unlockRead(stamp);
+            }
+        }
+        return shareData;
+    }
+}
